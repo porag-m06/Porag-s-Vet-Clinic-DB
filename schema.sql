f4c6d1381177576c548eca96d8cf0ab6f09db0ea
porag-m06 <porag.m06@gmail.com>
2023-08-12 19:09:32 +0600

    schema update

--- schema.sql
+++ schema.sql
@@ -13,3 +13,18 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
